b834966158f7ccae15fc836f70c27b52d99fe68e
winniez <xinyingzengvy@gmail.com>
2014-02-23 16:53:45 -0700

    Initial Commit

---
+++ hw6_ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hw6_ios
+//
+//  Created by Winnie Zeng on 2/23/14.
+//  Copyright (c) 2014 Winnie Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hw6_iosTests/hw6_iosTests.m
@@ -0,0 +1,34 @@
+//
+//  hw6_iosTests.m
+//  hw6_iosTests
+//
+//  Created by Winnie Zeng on 2/23/14.
+//  Copyright (c) 2014 Winnie Zeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hw6_iosTests : XCTestCase
+
+@end
+
+@implementation hw6_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
